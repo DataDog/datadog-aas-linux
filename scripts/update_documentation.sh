8341524436dbbd1e19427de3fda84ad041cbfd00
TophrC-dd <topher.campbell@datadoghq.com>
2024-05-30 14:00:27 -0400

    Updating action to handle the checkout of the documentation

--- scripts/update_documentation.sh
+++ scripts/update_documentation.sh
@@ -16,34 +16,22 @@ function print_color {
     printf "$GREEN$1$NC\n"
 }
 
-print_color "Creating a Github PR to update documentation"
-
-if [ ! -d $DOCUMENTATION_REPO_PATH ]; then
-    print_color "Documentation directory does not exist, cloning into $DOCUMENTATION_REPO_PATH"
-    git clone git@github.com:TophrC-dd/documentation $DOCUMENTATION_REPO_PATH
-fi
-
 cd $DOCUMENTATION_REPO_PATH
 
-# Make sure they don't have any local changes
+# Make sure they don't have any local changes - this shouldn't happen as the file is checked out on action execution
 if [ ! -z "$(git status --porcelain)" ]; then
     print_color "Documentation directory is dirty -- please stash or save your changes and manually create the PR"
     exit 1
 fi
 
-print_color "Pulling latest changes from Github"
-git checkout master
-git pull
-
 print_color "Checking out new branch that has version changes"
 git checkout -b $USER/bump-aas-wrapper-version-$VERSION
-sed -i '' -e '/.*datadog-aas-linux/{' -e 's/v[0-9]*.[0-9]*.[0-9]*/'"$VERSION"'/' -e '}' $DOCMENTATION_FILE
+sed -i '.' -e '/.*datadog-aas-linux/{' -e 's/v[0-9]*.[0-9]*.[0-9]*/'"$VERSION"'/' -e '}' $DOCUMENTATION_FILE
 git add $DOCUMENTATION_FILE
 
 print_color "Creating commit -- please tap your Yubikey if prompted"
 git commit -m "Bump AAS-WRAPPER to version $VERSION"
 git push --set-upstream origin $USER/bump-aas-wrapper-version-$VERSION
-dd-pr
 
 # Reset documentation repo to clean a state that's tracking master
 print_color "Resetting documentation git branch to master"
