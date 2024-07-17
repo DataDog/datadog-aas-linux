83d96a3be8b40f0f73b581585cf42b3626529a15
Aleksandr Pasechnik <aleksandr.pasechnik@datadoghq.com>
2024-07-17 16:58:59 -0400

    [SVLS-4918] can we talk to the internet?

--- .github/workflows/test_python.py
+++ .github/workflows/test_python.py
@@ -1,5 +1,11 @@
+from urllib.request import urlopen
+import json
+
 def main() -> None:
-    print("Hello, world")
+    with urlopen("https://api.github.com/repos/datadog/dd-trace-py/releases") as r:
+        data = json.loads(r.read().decode("utf-8"))
+
+        print(data)
 
 
 if __name__ == "__main__":
